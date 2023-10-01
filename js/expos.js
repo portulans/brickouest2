c0a010b7a2132974bbe432a70bfad7655884619d
portulans <solenn.tual@ensg.eu>
2023-10-01 08:44:00 +0200

    Update libraries

--- js/expos.js
+++ js/expos.js
@@ -4,10 +4,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties) {
+        var popup = "<h3>" + feature.properties.dates + "</h3><p><b>" + feature.properties.place + "</b><br>" + 
+                    feature.properties.street + "<br>" + 
+                    feature.properties.postcode + ' ' + feature.properties.city + '</p>'
+        layer.bindPopup(popup);
+    }
+}
+
 var url = "./data/brickouest.geojson"
 //https://umap.openstreetmap.fr/fr/map/brickouest_967088
 
 var expos = L.geoJSON(null, {
+    onEachFeature:onEachFeature
     });
     $.getJSON(url, function(data) {
         expos.addData(data);
