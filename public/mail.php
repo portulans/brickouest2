f8e75c124d1bf7f33335264d5bd2e8ca5c566c6f
portulans <solenn.tual@ensg.eu>
2023-10-01 10:35:48 +0200

    Update navbar

--- public/mail.php
+++ public/mail.php
@@ -10,10 +10,10 @@
     if (isset($_POST['message'])) {
         $entete  = 'MIME-Version: 1.0' . "\r\n";
         $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $entete .= 'From: webmaster@monsite.fr' . "\r\n";
+        $entete .= 'From: ' . $_POST['email'] . "\r\n";
         $entete .= 'Reply-to: ' . $_POST['email'];
 
-        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+        $message = '<h1>Message envoyé depuis la page Contact de brickouest.fr</h1>
         <p><b>Expéditeur : </b>' . $_POST['firstname'] . ' '. $_POST['firstname'] . '<br>
         <p><b>Email : </b>' . $_POST['email'] . '<br>
         <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
