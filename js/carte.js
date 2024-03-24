2c268146cc11edd9a74a83611ca0e5dfbb575042
portulans <solenn.tual@ensg.eu>
2024-03-24 12:39:18 +0100

    Update

--- js/carte.js
+++ js/carte.js
@@ -5,25 +5,87 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+function csvToGeojson(csvString) {
+    // Split CSV string into lines
+    var lines = csvString.split("\n");
+
+    // Extract header line
+    var headers = lines[0].split(";");
+
+    var features = [];
+
+    // Iterate over each line (starting from the second line, skipping header)
+    for (var i = 1; i < lines.length; i++) {
+        var line = lines[i].trim();
+        if (line !== "") {
+            // Split line into values
+            var values = line.split(";");
+
+            // Create a feature object
+            var feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [parseFloat(values[15]),parseFloat(values[14])]
+                },
+                "properties": {}
+            };
+
+            // Assign properties to the feature
+            for (var j = 0; j < headers.length; j++) {
+                if (j !== 'lat' && j !== 'long') {
+                    feature.properties[headers[j]] = values[j];
+                }
+            }
+
+            // Add feature to the features array
+            features.push(feature);
+        }
+    }
+
+    // Create a GeoJSON FeatureCollection
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    };
+
+    return geojson;
+}
+
+
 function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
     if (feature.properties) {
-        var popup = "<p><b>" + feature.properties.name + "</b><br>" + 
-                    feature.properties.street + "<br>" + 
-                    feature.properties.postcode + ' ' + feature.properties.city + '</p>'
+        var popup = "<p><b>" + feature.properties.titreevent + "</b><br>" + 
+                    feature.properties.adresse + "<br>" + 
+                    feature.properties.codepostal + ' ' + feature.properties.ville + '</p>'
         layer.bindPopup(popup);
     }
 }
 
-var url = "./data/lieux.geojson"
-//https://umap.openstreetmap.fr/fr/map/brickouest_967088
+// Define the URL of your CSV file
+var csvUrl = '././data/evenements.csv';
+var expos;
+// Fetch the CSV file
+fetch(csvUrl)
+    .then(response => response.text())
+    .then(csvString => {
+        //console.log(csvString)
+        // Convert the CSV string to GeoJSON
+        var geojson = csvToGeojson(csvString);
+        console.log(geojson)
+        var expos = L.geoJSON(geojson, {
+            onEachFeature:onEachFeature,
+            filter:function(feature,layer){
+                return feature.properties.statut === 'avenir'
+            }
+        });
+        
+        expos.addTo(map);
+    })
+    //.catch(error => console.error('Error fetching the CSV file:', error));
 
-var expos = L.geoJSON(null, {
-    onEachFeature:onEachFeature
-    });
-    $.getJSON(url, function(data) {
-        expos.addData(data);
-});
 
-expos.addTo(map);
+
+
 
